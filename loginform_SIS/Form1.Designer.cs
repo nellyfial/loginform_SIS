c544eaec66e55de6fa6716b84dabe64fac286992
ACT-STUDENT <ACT-STUDENT@LAB6-PC16>
2025-07-24 18:26:32 +0800

    hide pass and default unknown user

--- loginform_SIS/Form1.Designer.cs
+++ loginform_SIS/Form1.Designer.cs
@@ -34,15 +34,17 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             button1 = new Button();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(-34, -26);
+            pictureBox1.Location = new Point(-30, -20);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(457, 512);
+            pictureBox1.Size = new Size(400, 384);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
@@ -50,48 +52,64 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Arial Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.ForeColor = SystemColors.Desktop;
-            label1.Location = new Point(565, 58);
+            label1.Font = new Font("Arial Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.MediumSeaGreen;
+            label1.Location = new Point(428, 42);
             label1.Name = "label1";
-            label1.Size = new Size(95, 32);
+            label1.Size = new Size(220, 33);
             label1.TabIndex = 1;
-            label1.Text = "LOGIN";
+            label1.Text = "Welcome to SIS";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(492, 130);
-            textBox1.Multiline = true;
+            textBox1.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(430, 136);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
             textBox1.PlaceholderText = "Username:";
-            textBox1.Size = new Size(249, 47);
+            textBox1.Size = new Size(218, 22);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(492, 207);
-            textBox2.Multiline = true;
+            textBox2.Font = new Font("Arial", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(430, 182);
+            textBox2.Margin = new Padding(3, 2, 3, 2);
             textBox2.Name = "textBox2";
             textBox2.PlaceholderText = "Password:";
-            textBox2.Size = new Size(249, 47);
+            textBox2.Size = new Size(218, 22);
             textBox2.TabIndex = 5;
+            textBox2.UseSystemPasswordChar = true;
             // 
             // button1
             // 
             button1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(556, 282);
+            button1.Location = new Point(487, 229);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(129, 55);
+            button1.Size = new Size(106, 44);
             button1.TabIndex = 6;
             button1.Text = "Login";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.Desktop;
+            label2.Location = new Point(487, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 23);
+            label2.TabIndex = 7;
+            label2.Text = "Login now!";
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(label2);
             Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -99,6 +117,7 @@
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "LOGIN";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -113,5 +132,6 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Button button1;
+        private Label label2;
     }
 }
--- loginform_SIS/Form1.cs
+++ loginform_SIS/Form1.cs
@@ -15,28 +15,45 @@ namespace loginform_SIS
         {
             string username = this.textBox1.Text;
             string password = this.textBox2.Text;
+            textBox2.UseSystemPasswordChar = true;
 
-            string connectionString = "Data Source=5lgo8sik\\SQLEXPRESS;Initial Catalog=SIS;Integrated Security=True;TrustServerCertificate=True";
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SIS;Integrated Security=True;TrustServerCertificate=True";
 
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "SELECT * FROM student WHERE username = @username AND password = @password";
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@password", password);
-
                 try
                 {
                     conn.Open();
-                    SqlDataReader reader = cmd.ExecuteReader();
+                    string query = "SELECT role_id FROM users WHERE username = @username AND password = @password";
 
-                    if (reader.HasRows)
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    object result = cmd.ExecuteScalar();
+
+                    if (result != null)
                     {
-                        MessageBox.Show("Login successfully!");
+                        int role_Id = Convert.ToInt32(result);
+                        switch (role_Id)
+                        {
+                            case 1:
+                                MessageBox.Show("1, Hello! Welcome Admin!");
+                                break;
+                            case 2:
+                                MessageBox.Show("2, Hello! Welcome Student!");
+                                break;
+                            case 3:
+                                MessageBox.Show("3, Hello! Welcome Teacher!");
+                                break;
+                            default:
+                                MessageBox.Show("Unknown User:(");
+                                break;
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Invalid username or password. Please try again.");
+                        MessageBox.Show("Invalid Username or Password. Please try again.");
                     }
                 }
                 catch (Exception ex)
